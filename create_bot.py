c03b78dfe4b6a3141bc0943490b1adc789a9ece8
Andrew Utkin <106377120+AndrushaUt@users.noreply.github.com>
2022-12-13 05:10:04 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import psycopg
 storage = MemoryStorage()
-bot = Bot(token='5438351319:AAFxysJ5uGOYwVn23v8pPgXdkkz-kplKwCc')
+bot = Bot(token='5781824062:AAFSemAC-j7jbBzE5XrYxCNbVAf5FFKagJY')
 dp = Dispatcher(bot, storage = storage)
 connection = psycopg.connect(
     host='127.0.0.1',
